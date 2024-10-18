0645e62076e85dd66c355002cae5a0016b67b450
Daniel Niechaj <181674388+wiesiekor23@users.noreply.github.com>
2024-10-18 22:45:19 +0100

    Add more functions
    -Add Buttons to reset grid and set color
    to black(black button functionality not implemented yet).
    -Reset button prompts user for grid size and removes
    previous grid (associated function added).

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,23 +1,76 @@
-let usrInput = 4;
-let i = 1;
-let a = 1;
+let usrInput = 16;
+const controlPanel = document.querySelector("#control-panel");
 
-while (i <= usrInput) {
-    const container = document.querySelector("#container");
-    const rowDiv = document.createElement("div");
-    rowDiv.classList.add(`column${i}`);
-    rowDiv.classList.add("columnOfColumns")
-    rowDiv.textContent = "this is div nr:" + i;
-    container.appendChild(rowDiv);
-    while(a <= usrInput) {
-        const columnContainer = document.querySelector(`.column${i}`);
-        const columnDiv = document.createElement("div");
-        columnDiv.classList.add("row");
-        columnDiv.textContent = "this is row nr:" + a;
-        columnContainer.appendChild(columnDiv);
-        a++;
+const blackButton = document.createElement("button");
+const resetButton = document.createElement("button");
+blackButton.textContent = "Paint it Black!";
+resetButton.textContent = "RESET";
+controlPanel.append(blackButton, resetButton);
+
+resetButton.addEventListener("click", () =>{
+    resetBox();
+    usrInput = prompt("Give me a number from 1 to 100");
+    createGrid();
+    //black();
+    setColors();
+})
+
+
+function createGrid() {
+    let i = 1;
+    let a = 1;
+    while (i <= usrInput) {
+        const container = document.querySelector("#container");
+        const rowDiv = document.createElement("div");
+        rowDiv.classList.add(`column${i}`);
+        rowDiv.classList.add("columnOfColumns")
+        container.appendChild(rowDiv);
+        while(a <= usrInput) {
+            const columnContainer = document.querySelector(`.column${i}`);
+            const columnDiv = document.createElement("div");
+            columnDiv.classList.add("row");
+            columnContainer.appendChild(columnDiv);
+            a++;
+        }
+        a = 1;
+        i++;
     }
-    a = 1;
-    i++;
 }
 
+
+function setBlack() {
+    const rows = document.querySelectorAll(".row");
+    rows.forEach((row) => {
+        row.addEventListener("mouseover", () => {
+            row.setAttribute("style", "background: black;");
+        })
+    })
+};
+
+
+function setColors() {
+    console.log(random256);
+    const rows = document.querySelectorAll(".row");
+    rows.forEach((row) => {
+        row.addEventListener("mouseover", () => {
+            row.setAttribute("style", `background: 
+                rgb(${Math.floor(Math.random() * 257)}, 
+                ${Math.floor(Math.random() * 257)}, 
+                ${Math.floor(Math.random() * 257)});`);
+        })
+    })
+};
+
+function resetBox() {
+    const columns = document.querySelectorAll(".columnOfColumns")
+    columns.forEach((column) => {
+        column.remove();
+    });
+};
+
+function userInput () {
+
+}
+
+createGrid();
+setBlack();
