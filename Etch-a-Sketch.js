b9d3e8f3cd87503bdcdb9b8bda3e68819ec623bb
Daniel Niechaj <181674388+wiesiekor23@users.noreply.github.com>
2024-10-19 18:29:47 +0100

    Add opacity effect to black color.
    -Add 'Darken' button and it's functionality to gradually
    darken grid by 0.1 opacity on each interaction up to 1.0.
    -Deactivation function added to remove opacity effect being
    permanent when switched to color buttons.

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,52 +1,70 @@
 let usrInput = 16;
+let darkenActive = true;
+let colorActive = true;
+let blackActive = true;
+
+const rows = document.querySelectorAll(".row");
+
 const controlPanel = document.querySelector("#control-panel");
 
 const blackButton = document.createElement("button");
 const resetButton = document.createElement("button");
 const colorButton = document.createElement("button");
-blackButton.textContent = "Paint it Black!";
+const darkeningButton = document.createElement("button");
+blackButton.textContent = "Paint it, Black!";
 resetButton.textContent = "Reset";
-colorButton.textContent = "Colors"
-controlPanel.append(resetButton, blackButton, colorButton);
+colorButton.textContent = "Colors";
+darkeningButton.textContent = "Darken";
+controlPanel.append(resetButton, blackButton, colorButton, darkeningButton);
 
-    resetButton.addEventListener("click", () =>{
-        resetGrid();
-        usrInput = prompt("Give me a number from 1 to 100");
-        createGrid();
+resetButton.addEventListener("click", () => {
+    resetGrid();
+    usrInput = prompt("Give me a number from 1 to 100");
+    createGrid();
 });
 
-blackButton.addEventListener("click", () =>{
+blackButton.addEventListener("click", () => {
+    //darkenActive = false;
     setBlack();
+    deactivateDBlack();
 });
 
 colorButton.addEventListener("click", () => {
+    //darkenActive = false;
     setColors();
+    deactivateDBlack();
 });
+
+darkeningButton.addEventListener("click", () => {
+    //darkenActive = true;
+    //darkenBlack();
+    activateDBlack();
+})
 
 
 function createGrid() {
     let i = 1;
     let a = 1;
     if (usrInput <= 100 && usrInput > 0) {
-    while (i <= usrInput) {
-        const container = document.querySelector("#container");
-        const rowDiv = document.createElement("div");
-        rowDiv.classList.add(`column${i}`);
-        rowDiv.classList.add("columnOfColumns")
-        container.appendChild(rowDiv);
-        while(a <= usrInput) {
-            const columnContainer = document.querySelector(`.column${i}`);
-            const columnDiv = document.createElement("div");
-            columnDiv.classList.add("row");
-            columnContainer.appendChild(columnDiv);
-            a++;
+        while (i <= usrInput) {
+            const container = document.querySelector("#container");
+            const rowDiv = document.createElement("div");
+            rowDiv.classList.add(`column${i}`);
+            rowDiv.classList.add("columnOfColumns")
+            container.appendChild(rowDiv);
+            while (a <= usrInput) {
+                const columnContainer = document.querySelector(`.column${i}`);
+                const columnDiv = document.createElement("div");
+                columnDiv.classList.add("row");
+                columnContainer.appendChild(columnDiv);
+                a++;
+            }
+            a = 1;
+            i++;
         }
-        a = 1;
-        i++;
-    }
-} else {
-    alert("Number too big or too small!!! - Press 'Reset' button to try again");
-};
+    } else {
+        alert("Number too big or too small!!! - Press 'Reset' button to try again");
+    };
 }
 
 
@@ -55,6 +73,7 @@ function setBlack() {
     const rows = document.querySelectorAll(".row");
     rows.forEach((row) => {
         if (row.style.backgroundColor === "") {
+            //row.style.opacity = 1;
             row.addEventListener("mouseover", function cancelHandler() {
                 row.setAttribute("style", "background: black;");
                 row.removeEventListener("mouseover", cancelHandler);
@@ -68,6 +87,7 @@ function setColors() {
     const rows = document.querySelectorAll(".row");
     rows.forEach((row) => {
         if (row.style.backgroundColor === "") {
+            //row.style.opacity = 1;
             row.addEventListener("mouseover", function cancelHandler() {
                 row.setAttribute("style", `background: 
                     rgb(${Math.floor(Math.random() * 257)}, 
@@ -79,16 +99,59 @@ function setColors() {
         });
     };
     
-    function resetGrid() {
-        const columns = document.querySelectorAll(".columnOfColumns")
-        columns.forEach((column) => {
-            column.remove();
-        });
-    };
     
-    function userInput () {
+    
+function resetGrid() {
+    const columns = document.querySelectorAll(".columnOfColumns")
+    columns.forEach((column) => {
+        column.remove();
+    });
+};
 
-    }
-    
-    createGrid();
+/*function darkenBlack() {
+    const rows = document.querySelectorAll(".row");
     setBlack();
+    rows.forEach((row) => {
+        //row.style.opacity = 0;
+        if (row.style.backgroundColor === "") {
+            row.addEventListener("mouseover", function cancelHandler() {
+                if (row.style.opacity < 1) {
+                    row.style.opacity -= "-0.1";
+                } else if (row.style.opacity === 1) {
+                    row.removeEventListener("mouseover", cancelHandler);
+                };
+            });
+        };
+    });
+};*/
+
+function activateDBlack() {
+    const rows = document.querySelectorAll(".row");
+    setBlack();
+    rows.forEach((row) => {
+        //row.style.opacity = 0; // Reset opacity
+        if (row.style.backgroundColor === "") {
+            row.addEventListener("mouseover", dBlack);
+        }
+    });
+}
+
+function deactivateDBlack() {
+    const rows = document.querySelectorAll(".row");
+    rows.forEach((row) => {
+        if (row.style.backgroundColor === "") {
+            row.removeEventListener("mouseover", dBlack);
+        }
+    });
+}
+
+
+function dBlack(event) {
+    const row = event.currentTarget;
+    if (row.style.opacity < 1) {
+        row.style.opacity -=  "-0.1";
+    }
+}
+
+createGrid();
+//setBlack();
